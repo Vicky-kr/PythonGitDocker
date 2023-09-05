a9b25429615a137c78a308fa28b02ba167c04268
Vicky Kumar <77744051+Vicky-kr@users.noreply.github.com>
2023-09-05 16:21:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello This is a python Docker Test"
-
+    return "Hello This is a python Docker Test!!"
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
  
